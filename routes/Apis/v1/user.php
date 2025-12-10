ea652a2249318bc73555af81f0ceb1a2bed64bf4
brayanmedinacardozo96 <brayanmedinacardozo@gmail.com>
2025-12-09 19:30:47 -0500

    feat: ajustar validacion de email

--- routes/Apis/v1/user.php
+++ routes/Apis/v1/user.php
@@ -9,6 +9,6 @@ Route::prefix('v1')->name('api.v1.')->group(function () {
     Route::prefix('user')->name('user.')->group(function () {
         Route::post('/', CreateUserController::class)->name('create');
         Route::post('/email-verification', EmailVerificationController::class)->name('email.verify');
-        Route::post('/verification/code', CodeVerificationController::class)->name('verification.code');
+        Route::post('/verification-code', CodeVerificationController::class)->name('verification.code');
     });
 });
